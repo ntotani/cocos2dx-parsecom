9aaafc21d73a5fd0be4d11dbb1eae8bd70f28730
Naoyuki Totani <n_totani@septeni-original.co.jp>
2014-08-12 13:20:13 +0900

    fix protocol user

--- frameworks/cocos2d-x/plugin/protocols/platform/ios/PluginFactory.mm
+++ frameworks/cocos2d-x/plugin/protocols/platform/ios/PluginFactory.mm
@@ -26,6 +26,7 @@ THE SOFTWARE.
 #include "ProtocolAnalytics.h"
 #include "ProtocolIAP.h"
 #include "ProtocolShare.h"
+#include "ProtocolUser.h"
 #include "PluginUtilsIOS.h"
 
 #import <Foundation/Foundation.h>
@@ -33,6 +34,7 @@ THE SOFTWARE.
 #import "InterfaceAnalytics.h"
 #import "InterfaceIAP.h"
 #import "InterfaceShare.h"
+#import "InterfaceUser.h"
 
 namespace cocos2d { namespace plugin {
 
@@ -95,8 +97,11 @@ PluginProtocol* PluginFactory::createPlugin(const char* name)
         if ([obj conformsToProtocol:@protocol(InterfaceIAP)]) {
             pRet = new ProtocolIAP();
         } else
-        if ([obj conformsToProtocol:@protocol(InterfaceShare)]) {
-            pRet = new ProtocolShare();
+            if ([obj conformsToProtocol:@protocol(InterfaceShare)]) {
+                pRet = new ProtocolShare();
+        } else
+            if ([obj conformsToProtocol:@protocol(InterfaceUser)]) {
+                pRet = new ProtocolUser();
         } else {
             PluginUtilsIOS::outputLog("Plugin %s not implements a right protocol", name);
         }
